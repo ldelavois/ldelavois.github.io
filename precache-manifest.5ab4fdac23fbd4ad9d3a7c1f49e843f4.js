0b651f05b355ae40f6425a58e4b6ca13a66c4f66
ldelavois <ludovic.delavois@hotmail.fr>
2020-11-16 16:59:30 +0100

    Updates

---
+++ precache-manifest.5ab4fdac23fbd4ad9d3a7c1f49e843f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b30285767b8d09c274e36899bc3457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860318f715040e724290",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b218a464e3cc857b6c83",
+    "url": "/static/css/main.6c80914b.chunk.css"
+  },
+  {
+    "revision": "860318f715040e724290",
+    "url": "/static/js/2.39e4a205.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.39e4a205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b218a464e3cc857b6c83",
+    "url": "/static/js/main.974651cf.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
